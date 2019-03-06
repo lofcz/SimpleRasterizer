adc792434a0d3f9a3134e6ea07867105cf4e1cd5
Matěj Štágl <stagl.matej@centrum.cz>
2019-03-06 15:57:49 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BresenhamTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(639, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(639, 40);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(601, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(601, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BresenhamTest
+{
+    public partial class Form1 : Form
+    {
+        private Brush b = new SolidBrush(Color.Red);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // atomic
+            PlotEllipse(Width / 2, Height / 2, (int)numericUpDown1.Value, (int)numericUpDown2.Value, e);
+            PlotLine(100, 100, 10, 10, e);
+            PlotArc(200, 200, 48, 40, 180, 24, e);
+
+            // shortcuts
+            PlotRectangle(150, 50, 190, 90, e);
+            PlotTriangle(200, 200, 250, 200, 225, 180, e);
+            PlotRoundedRect(250, 250, 350, 350, 32, e);
+        }
+
+        private double degtorad(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        void PlotRoundedRect(int x1, int y1, int x2, int y2, int rounded, PaintEventArgs e)
+        {
+            PlotLine(x1 + rounded, y1, x2 - rounded, y1, e);
+            PlotLine(x1 + rounded, y2, x2 - rounded, y2, e);
+            PlotLine(x1, y1 + rounded, x1, y2 - rounded, e);
+            PlotLine(x2, y1 + rounded, x2, y2 - rounded, e);
+
+            PlotArc(x1 + rounded, y1 + rounded, rounded, 180, 90, 32, e);
+            PlotArc(x2 - rounded, y1 + rounded, rounded, 270, 90, 32, e);
+            PlotArc(x1 + rounded, y2 - rounded, rounded, 90, 90, 32, e);
+            PlotArc(x2 - rounded, y2 - rounded, rounded, 0, 90, 32, e);
+        }
+
+        void PlotTriangle(int x1, int y1, int x2, int y2, int x3, int y3, PaintEventArgs e)
+        {
+            PlotLine(x1, y1, x2, y2, e);
+            PlotLine(x1, y1, x3, y3, e);
+            PlotLine(x2, y2, x3, y3, e);
+        }
+
+        void PlotRectangle(int x1, int y1, int x2, int y2, PaintEventArgs e)
+        {
+            PlotLine(x1, y1, x2, y1, e);
+            PlotLine(x1, y1, x1, y2, e);
+            PlotLine(x2, y1, x2, y2, e);
+            PlotLine(x2, y2, x1, y2, e);
+        }
+
+        void PlotArc(int cx, int cy, int r, float startAngle, float arcAngle, int segments, PaintEventArgs e)
+        {
+            float theta = (6.283185f / segments) * (arcAngle / 360f);
+            float tv = (float) Math.Tan(theta);
+            float rv = (float) Math.Cos(theta);
+
+            float x = r * (float) Math.Cos(degtorad(startAngle));
+            float y = r * (float) Math.Sin(degtorad(startAngle));
+
+            int prevX = (int) (x + cx);
+            int prevY = (int) (y + cy);
+
+            int nextX = (int)(x + cx);
+            int nextY = (int)(y + cy);
+
+            for (int i = 0; i < segments; i++)
+            {
+                nextX = (int)(x + cx);
+                nextY = (int)(y + cy);
+
+                PlotLine(prevX, prevY, nextX, nextY, e);
+
+                float tx = -y;
+                float ty = x;
+
+                x += tx * tv;
+                y += ty * tv;
+
+                x *= rv;
+                y *= rv;
+
+                prevX = nextX;
+                prevY = nextY;
+            }
+
+            PlotLine(prevX, prevY, nextX, nextY, e);
+        }
+
+        void PlotLine(int x1, int y1, int x2, int y2, PaintEventArgs e)
+        {
+            int dx, dy, sx, sy, e2, err;
+
+            dx = Math.Abs(x2 - x1);
+            dy = Math.Abs(y2 - y1);
+            err = 0;
+
+            sx = x1 < x2 ? 1 : -1;
+            sy = y1 < y2 ? 1 : -1;
+
+            for (;;)
+            {
+                putPixel(x1, y1, e);
+
+                if (x1 == x2 && y1 == y2)
+                {
+                    break;
+                }
+
+                e2 = err;
+
+                if (e2 > -dx)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+
+                if (e2 < dy)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+        }
+
+        void PlotEllipse(int cx, int cy, int xrad, int yrad, PaintEventArgs e)
+        {
+            int x = 0, y = 0, xchange = 0, ychange = 0, ellipseError = 0, TwoASquare = 0, TwoBSquare = 0, StoppingX = 0, StoppingY = 0;
+
+            TwoASquare = 2 * xrad * xrad;
+            TwoBSquare = 2 * yrad * yrad;
+            x = xrad;
+            y = 0;
+
+            xchange = yrad * yrad * (1 - 2 * xrad);
+            ychange = xrad * xrad;
+
+            ellipseError = 0;
+
+            StoppingX = TwoBSquare * xrad;
+            StoppingY = 0;
+
+            while (StoppingX >= StoppingY)
+            {
+                Plot4EllipsePoints(x, y);
+                y++;
+                StoppingY += TwoASquare;
+                ellipseError += ychange;
+                ychange += TwoASquare;
+
+                if (2 * ellipseError + xchange > 0)
+                {
+                    x--;
+                    StoppingX -= TwoBSquare;
+                    ellipseError += xchange;
+                    xchange += TwoBSquare;
+                }
+            }
+
+            x = 0;
+            y = yrad;
+
+            xchange = yrad * yrad;
+            ychange = xrad * xrad * (1 - 2 * yrad);
+            ellipseError = 0;
+            StoppingX = 0;
+            StoppingY = TwoASquare * yrad;
+
+            while (StoppingX <= StoppingY)
+            {
+                Plot4EllipsePoints(x, y);
+                x++;
+                StoppingX += TwoBSquare;
+                ellipseError += xchange;
+                xchange += TwoBSquare;
+
+                if (2 * ellipseError + ychange > 0)
+                {
+                    y--;
+                    StoppingY -= TwoASquare;
+                    ellipseError += ychange;
+                    ychange += TwoASquare;
+                }
+            }
+
+
+            void Plot4EllipsePoints(int xx, int yy)
+            {
+                putPixel(cx + x, cy + y, e);
+                putPixel(cx - x, cy + y, e);
+                putPixel(cx - x, cy - y, e);
+                putPixel(cx + x, cy - y, e);
+            }
+        }
+
+
+
+        void putPixel(int x, int y, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(b, x, y, 1, 1);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            Invalidate();
+        }
+    }
+}
